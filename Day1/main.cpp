b36c969a20e8cbcd85d05735546e1471296f175f
Marcus Krahl <mail@marcuskrahl.de>
2015-12-01 17:59:36 +0100

    added the main loop code to run get_destination_floor

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 
+#include "get_destination_floor.hpp"
 
 int main(int argc, char* argv[]) {
+    std::string movement;
+    std::cin >> movement;
+    int final_floor = get_destination_floor(movement);
+    std::cout << final_floor << std::endl;
     return 0;
 }
