d9da0fc98d6d8d181847732a8d4d99d9fafa8d56
Marcus Krahl <mail@marcuskrahl.de>
2015-12-01 18:32:14 +0100

    implemented call of get_basement_position in main

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -3,10 +3,29 @@
 
 #include "floor_functions.hpp"
 
-int main(int argc, char* argv[]) {
+void run_part_one() {
     std::string movement;
     std::cin >> movement;
     int final_floor = get_destination_floor(movement);
     std::cout << final_floor << std::endl;
-    return 0;
+
+}
+void run_part_two() {
+    std::string movement;
+    std::cin >> movement;
+    auto basement_entered_at = get_basement_position(movement);
+    std::cout << basement_entered_at << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        if (std::string(argv[1]) == "--part_two") {
+            run_part_two();
+        } else {
+            std::cout << "Usage: day1 [--part_two]" << std::endl;
+            return -1;
+        }
+    } else {
+        run_part_one();
+    }
 }
