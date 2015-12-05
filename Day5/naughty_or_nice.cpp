ab99d18d4bda2d048f4839adf2002fb8b563597d
Marcus Krahl <mail@marcuskrahl.de>
2015-12-05 12:17:07 +0100

    added check for double letters

--- Day5/naughty_or_nice.cpp
+++ Day5/naughty_or_nice.cpp
@@ -23,6 +23,15 @@ bool has_at_least_three_vowels(const std::string& input) {
     return number_of_vowels >= 3;
 }
 
+bool has_at_least_one_double_letter(const std::string& input) {
+    for (auto it = input.begin()+1; it != input.end(); it++) {
+        if( *(it-1) == *it) {
+            return true;
+        }
+    }
+    return false;
+}
+
 bool is_nice(const std::string& input) {
-    return has_at_least_three_vowels(input);
+    return has_at_least_three_vowels(input) && has_at_least_one_double_letter(input);
 }
--- Day5/tests.cpp
+++ Day5/tests.cpp
@@ -6,5 +6,11 @@
 TEST_CASE("Word is nice if it has at least three vowels","[naughty or nice]") {
     REQUIRE(is_nice("") == false);
     REQUIRE(is_nice("ae") == false);
-    REQUIRE(is_nice("aeu") == true);
+    REQUIRE(is_nice("aee") == true); 
+}
+
+TEST_CASE("Word is nice if it has at least one double letter","[naughty or nice]") {
+    REQUIRE(is_nice("aeu") == false);
+    REQUIRE(is_nice("aeuu") == true);
+    REQUIRE(is_nice("addeu") == true);
 }
