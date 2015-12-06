08855bbb87c27d13aef7cef335ab9bf981b8f16f
Marcus Krahl <mail@marcuskrahl.de>
2015-12-06 18:05:57 +0100

    added runtime code for part one of day 6

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
 
+#include "LightCommand.hpp"
+#include "LightMap.hpp"
+
 void run_part_one() {
+    LightMap light_map;
+    std::string command;
+    while (std::getline(std::cin,command)) {
+        light_map.apply_command(*LightCommand::from_input(command));
+    }
+    std::cout << light_map.get_active_lights() << std::endl;
 }
 void run_part_two() {
 }
