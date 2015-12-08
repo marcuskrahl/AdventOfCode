1e60eb2721620c756f2541f7d9f98b9d29b4c35d
Marcus Krahl <mail@marcuskrahl.de>
2015-12-08 17:03:48 +0100

    refactored memory_length()

--- Day8/Santa_StringLength.cpp
+++ Day8/Santa_StringLength.cpp
@@ -4,16 +4,27 @@ size_t string_length(const std::string& input) {
     return input.length();
 }
 
+bool is_escaped_char(const char& char_to_escape, std::string::const_iterator& position, const std::string::const_iterator& end) {
+    if (*position != '\\') {
+        return false;
+    }
+    auto next_char_position = position+1;
+    if (next_char_position == end) {
+        return false;
+    }
+    if (*next_char_position != char_to_escape) {
+        return false;
+    }
+    return true;
+}
+
 size_t memory_length(const std::string& input) {
     size_t length = 0;
     size_t surrounding_quotes = 2;
     for ( auto it = input.begin(); it != input.end(); it++) {
-       if ((*it == '\\') && ((it+1) != input.end()) && (*(it+1) == '\\')) {
+       if (is_escaped_char('\\',it,input.end()) || is_escaped_char('\"',it,input.end())) {
            length += 1;
            it++;
-       } else if ((*it == '\\') && ((it+1) != input.end()) && (*(it+1) == '\"')) {
-          length += 1;
-          it++;
        } else {
             length += 1;
        }
