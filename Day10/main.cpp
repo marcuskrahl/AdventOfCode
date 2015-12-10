0f8de1f4ad8e30506a2976487c6250847abdff54
Marcus Krahl <mail@marcuskrahl.de>
2015-12-10 17:28:00 +0100

    added runtime code for day 10 part one

--- Day10/main.cpp
+++ Day10/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <string>
 
+#include "LookAndSay.hpp"
+
 void run_part_one() {
+    std::string value = "1113222113";
+    for (unsigned int i = 0; i< 40; i++) {
+        value = look_and_say(value);
+    }
+    std::cout << value.length() << std::endl;
 }
 void run_part_two() {
 }
