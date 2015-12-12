060b534dbdea35fbe38b000c1fec05add7bc9d51
Marcus Krahl <mail@marcuskrahl.de>
2015-12-12 18:10:31 +0100

    added runtime code for day 12 part two

--- Day12/main.cpp
+++ Day12/main.cpp
@@ -9,6 +9,9 @@ void run_part_one() {
     std::cout << get_sum_of_numbers(json_string) << std::endl;
 }
 void run_part_two() {
+    std::string json_string;
+    std::cin >> json_string;
+    std::cout << get_sum_of_numbers_without_red(json_string) << std::endl;
 }
 
 
