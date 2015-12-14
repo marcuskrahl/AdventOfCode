b50f43f2ce32ac20456552599436031eea4dae1f
Marcus Krahl <mail@marcuskrahl.de>
2015-12-14 20:48:12 +0100

    added runtime code for day 14 part two

--- Day14/main.cpp
+++ Day14/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
 
 #include "Reindeer.hpp"
 
@@ -15,6 +18,33 @@ void run_part_one() {
     std::cout << max_distance << std::endl;
 }
 void run_part_two() {
+    std::vector<Reindeer> reindeers;
+    std::vector<int> scores;
+    std::string line;
+    while(std::getline(std::cin,line)) {
+        auto reindeer = Reindeer::from_string(line);
+        reindeers.push_back(reindeer);
+        scores.push_back(0);
+    }
+    for (unsigned int seconds = 1; seconds <= 2503; seconds++) {
+        unsigned int max_travelled_distance = 0;
+        std::vector<int> max_indices;
+        for(size_t i=0; i<reindeers.size();i++) {
+            unsigned int travelled_distance = reindeers[i].get_distance_after_seconds(seconds);
+            if (travelled_distance > max_travelled_distance) {
+                max_travelled_distance = travelled_distance;
+                max_indices.empty();
+                max_indices.push_back(i);
+            } else if (travelled_distance == max_travelled_distance) {
+                max_indices.push_back(i);
+            }
+        }
+        for (auto i: max_indices) {
+            scores[i]++; 
+        }
+    }
+    unsigned int max_score = *std::max_element(scores.begin(),scores.end());
+    std::cout << max_score << std::endl;
 }
 
 
