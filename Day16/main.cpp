a2eee5c1b37c8b40a057600c875da9762b3534bc
Marcus Krahl <mail@marcuskrahl.de>
2015-12-16 16:22:27 +0100

    added project for day 16

---
+++ Day16/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+void run_part_one() {
+}
+void run_part_two() {
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        if (std::string(argv[1]) == "--part_two") {
+            run_part_two();
+        } else {
+            std::cout << "Usage: day16 [--part_two]" << std::endl;
+            return -1;
+        }
+    } else {
+        run_part_one();
+    }
+}
