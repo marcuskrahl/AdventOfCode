6d624eefc6eb9458336a4d8ee4516e8b070d9216
Marcus Krahl <mail@marcuskrahl.de>
2015-12-17 17:41:31 +0100

    added target depth to search for containers

--- Day17/Containers.cpp
+++ Day17/Containers.cpp
@@ -1,6 +1,12 @@
 #include "Containers.hpp"
 
-unsigned get_sub_number_of_combinations(std::vector<unsigned int>::const_iterator begin, const std::vector<unsigned int>::const_iterator end, unsigned int target_value) {
+#include <limits>
+
+unsigned get_sub_number_of_combinations(std::vector<unsigned int>::const_iterator begin, const std::vector<unsigned int>::const_iterator end, unsigned int target_value, int current_depth, int target_depth) {
+    current_depth++;
+    if (current_depth > target_depth) {
+        return 0;
+    }
     unsigned int number_of_combinations = 0;
     if (begin == end) {
         return 0;
@@ -11,7 +17,7 @@ unsigned get_sub_number_of_combinations(std::vector<unsigned int>::const_iterato
         } else if (*it == target_value) {
             number_of_combinations++;
         } else {
-            number_of_combinations += get_sub_number_of_combinations(it+1,end,target_value - *it);
+            number_of_combinations += get_sub_number_of_combinations(it+1,end,target_value - *it, current_depth, target_depth);
         }
     }
     return number_of_combinations;
@@ -19,5 +25,9 @@ unsigned get_sub_number_of_combinations(std::vector<unsigned int>::const_iterato
 }
 
 unsigned int get_number_of_combinations(std::vector<unsigned int> containers, unsigned int target_value) {
-    return get_sub_number_of_combinations(containers.begin(),containers.end(), target_value);
+    return get_sub_number_of_combinations(containers.begin(),containers.end(), target_value,0,std::numeric_limits<int>::max());
+}
+
+unsigned int get_number_of_combinations_with_depth(std::vector<unsigned int> containers, unsigned int target_value, int target_depth) {
+    return get_sub_number_of_combinations(containers.begin(),containers.end(), target_value,0,target_depth);
 }
--- Day17/tests.cpp
+++ Day17/tests.cpp
@@ -10,3 +10,8 @@ TEST_CASE("All combinations of containers are found","[containers]") {
     REQUIRE(get_number_of_combinations(containers,25) == 4);
 
 }
+
+TEST_CASE("All combinations with a given depth are found","[containers]") {
+    std::vector<unsigned int> containers = {20,15,10,5,5};
+    REQUIRE(get_number_of_combinations_with_depth(containers,25,2) == 3);
+}
