196ea663571a287a0002e345edabbfdb907e7b5c
Marcus Krahl <mail@marcuskrahl.de>
2015-12-21 16:33:03 +0100

    fixed an issue where the minimal damage of 1 was not respected

--- Day21/BossFight.cpp
+++ Day21/BossFight.cpp
@@ -2,13 +2,13 @@
 
 bool boss_fight(unsigned int player_health, unsigned int player_damage,unsigned int player_armor,unsigned int monster_health,unsigned int monster_damage, unsigned int monster_armor) {
     while(true) {
-        unsigned int dealt_damage = player_damage - monster_armor;
+        unsigned int dealt_damage = player_damage <= monster_armor ? 1 : player_damage - monster_armor;
         if (dealt_damage >= monster_health) { 
             return true;     
         } else {
             monster_health -= dealt_damage;
         }
-        dealt_damage = monster_damage - player_armor;
+        dealt_damage = monster_damage <= player_armor ? 1 : monster_damage - player_armor;
         if (dealt_damage >= player_health) { 
             return false;     
         } else {
