67a46e7006aefe193749f748378832ac6e05c22f
Marcus Krahl <mail@marcuskrahl.de>
2015-12-21 15:58:51 +0100

    added boss fight calculation

---
+++ Day21/BossFight.hpp
@@ -0,0 +1,5 @@
+#ifndef BOSS_FIGHT_HPP
+#define BOSS_FIGHT_HPP
+
+bool boss_fight(unsigned int player_health, unsigned int player_damage,unsigned int player_armor,unsigned int monster_health,unsigned int monster_damage, unsigned int monster_armor);
+#endif
