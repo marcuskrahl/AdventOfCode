448fb98297ce9d2fd11da31b72b1fd41d11c5c6b
Marcus Krahl <mail@marcuskrahl.de>
2015-12-22 16:47:02 +0100

    added project for day 22

---
+++ Day22/tests.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+#include "../lib/catch.hpp"
+
+TEST_CASE("Magic fight","[magic fight]") {
+    // There are no unit tests for this day because the implementation cost of these tests
+    // is higher than the benefit in this specific test case
+    REQUIRE (1==1);
+}
