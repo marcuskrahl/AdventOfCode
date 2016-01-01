4878a5933ea365c434f34fda13c009e625453ac5
Marcus Krahl <mail@marcuskrahl.de>
2016-01-01 17:01:50 +0100

    added runtime code for day 23 part one

--- Day23/main.cpp
+++ Day23/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <string>
+
+#include "Instructions.hpp"
  
 void run_part_one() {
+    std::string line;
+    InstructionRunner runner;
+    std::array<int,REGISTER_COUNT> registers = {0,0};
+    while(std::getline(std::cin,line)) {
+        runner.add_instruction(parse_instruction(line));
+    }
+    runner.run(registers);
+    std::cout<<registers[1]<<std::endl;
 }
 void run_part_two() {
 }
