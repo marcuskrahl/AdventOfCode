4fe210bfa2b9acbe4e551f575a04aff9898f006e
Marcus Krahl <mail@marcuskrahl.de>
2016-01-03 14:54:08 +0100

    changed recursive code algorithm to iterative

--- Day25/CodeGeneration.cpp
+++ Day25/CodeGeneration.cpp
@@ -1,11 +1,12 @@
 #include "CodeGeneration.hpp"
 
 unsigned long get_code(unsigned int n) {
-    if (n == 1) {
-        return 20151125ul;
-    } else {
-        return (get_code(n-1) * 252533ul) % 33554393ul;
+    unsigned long code = 20151125ul;
+    while (n-- > 1) {
+        code = (code * 252533ul) % 33554393ul;
+
     }
+    return code;
 }
 
 unsigned long get_code(unsigned int row, unsigned int column) {
