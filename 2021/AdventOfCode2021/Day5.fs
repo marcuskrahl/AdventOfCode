0fa4714a76f5f8edfd043ac03f5180089afbcde1
Marcus Krahl <mail@marcuskrahl.de>
2021-12-05 15:38:30 +0100

    add faster solution for day 5

--- 2021/AdventOfCode2021/Day5.fs
+++ 2021/AdventOfCode2021/Day5.fs
@@ -22,7 +22,7 @@ let matchesMovement (x,y) movement =
         ((x >= x1 && x <= x2) || (x >= x2 && x <= x1)) && ((y >= y1 && y <= y2) || (y >= y2 && y <= y1))  
 
 let sign x = 
-    if x < 0 then -1 else 1
+    if x < 0 then -1 else if x > 0 then 1 else 0
 
 let matchesMovementDiag (x,y) movement = 
     let (x1,y1),(x2, y2) = movement
@@ -56,6 +56,17 @@ let printGrid grid =
 let countOverlaps grid = 
     grid |> List.map (fun row -> row |> List.map (fun v -> if v >= 2 then 1 else 0) |> List.sum) |> List.sum
 
+
+let rec generatePoints movement = 
+    let ((x1, y1), (x2,y2)) = movement
+    if x1 = x2 && y1 = y2 
+    then 
+        [ (x1, y1) ]
+    else
+        let xDiff = sign (x2 - x1)
+        let yDiff = sign (y2 - y1)
+        (x1, y1) :: generatePoints ((x1 + xDiff, y1 + yDiff), (x2, y2))
+
 let part1 (input: string list) = 
     let movements = input |> List.map parseInput
     let grid = List.init 1000 (fun _ -> List.init 1000 (fun _ -> 0))
@@ -64,9 +75,16 @@ let part1 (input: string list) =
     countOverlaps filledGrid
 
 
-let part2 (input: string list) = 
+let part2_slow (input: string list) = 
     let movements = input |> List.map parseInput
     let grid = List.init 1000 (fun _ -> List.init 1000 (fun _ -> 0))
     let filledGrid = movements |> List.fold (fun acc m -> printfn "%A" m; fillGridDiag acc m) grid
     //let reducedGrid = filledGrid |> List.take 10 |> List.map (fun l -> l |> List.take 10)
     countOverlaps filledGrid
+
+let part2 (input: string list) = 
+    let movements = input |> List.map parseInput
+    let points = movements |> List.map generatePoints |> List.concat
+    let groupedPoints = points |> List.countBy (fun m -> m)
+    let result = groupedPoints |> List.map (fun (_, c) -> if c >= 2 then 1 else 0) |> List.sum
+    result
