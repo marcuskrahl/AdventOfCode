73391f260055a6c673afefeb5acfe567b0e57f6e
Marcus Krahl <mail@marcuskrahl.de>
2021-12-12 09:34:32 +0100

    solve day 11

--- 2021/AdventOfCode2021.Tests/Day11_Tests.fs
+++ 2021/AdventOfCode2021.Tests/Day11_Tests.fs
@@ -5,16 +5,25 @@ open Xunit
 open Day11
 
 let input = [
-
+        "5483143223";
+        "2745854711";
+        "5264556173";
+        "6141336146";
+        "6357385478";
+        "4167524645";
+        "2176841721";
+        "6882881134";
+        "4846848554";
+        "5283751526"
     ]
 
 
 [<Fact>]
 let ``Part 1 returns correct result`` () =
     let result = part1 input
-    Assert.Equal(42, result)
+    Assert.Equal(1656, result)
 
 [<Fact>]
 let ``Part 2 returns correct result`` () =
     let result = part2 input
-    Assert.Equal(42, result)
+    Assert.Equal(195, result)
--- 2021/AdventOfCode2021/Day11.fs
+++ 2021/AdventOfCode2021/Day11.fs
@@ -2,8 +2,74 @@ module Day11
 
 open System
 
+let parseCave (input: string list) =
+    let inputLength = input |> List.length
+    let heightMap = Array2D.create inputLength (input |> List.item 0).Length 0
+    input |> List.iteri (fun i line -> 
+        for j in 0..(line.Length - 1) do
+            heightMap.[i,j] <- Convert.ToInt32($"{line.[j]}")
+    )
+    heightMap
+
+
+let flashPos (cave: int[,]) i j =
+    cave.[i,j] <- 0
+    for iDelta in -1..1 do
+        for jDelta in -1..1 do
+            let newI = i + iDelta
+            let newJ = j + jDelta
+            if iDelta = 0 && jDelta = 0 then ()
+            else if newI < 0 || newJ < 0 then ()
+            else if newI >= Array2D.length1 cave || newJ >= Array2D.length2 cave then ()
+            else if cave.[newI, newJ] = 0 then ()
+            else 
+                cave.[newI,newJ] <- cave.[newI, newJ] + 1
+                ()
+
+let rec flashCave cave = 
+    let mutable flashes = 0;
+    for i in 0..((cave |> Array2D.length1) - 1) do
+        for j in 0..((cave |> Array2D.length2) - 1) do
+            if cave.[i,j] > 9 then
+                flashes <- flashes + 1
+                flashPos cave i j
+            else ()
+    if flashes > 0 then
+        flashes + flashCave cave
+    else
+        flashes
+
+let printCave cave = 
+    Console.SetCursorPosition(0,0)
+    for i in 0..((cave |> Array2D.length1) - 1) do
+        for j in 0..((cave |> Array2D.length2) - 1) do
+            Console.Write($"{cave.[i,j]}")
+        Console.WriteLine()
+    System.Threading.Thread.Sleep(300)
+
+let rec simulateCaveStep cave remainingSteps flashes =
+    if remainingSteps = 0 then
+        flashes
+    else
+    let increasedCave = Array2D.map (fun c -> c + 1) cave 
+    let newFlashes = flashCave increasedCave 
+    //printCave increasedCave
+    simulateCaveStep increasedCave (remainingSteps - 1) (flashes + newFlashes)
+
+let rec getAllFlash cave steps =
+    let increasedCave = Array2D.map (fun c -> c + 1) cave 
+    let newFlashes = flashCave increasedCave 
+    if newFlashes = (Array2D.length1 cave) * (Array2D.length2 cave) then
+        steps + 1
+    else
+       getAllFlash increasedCave (steps + 1)
+
 let part1 (input: string list) = 
-    42
+    let cave = parseCave input
+    let result = simulateCaveStep cave 100 0
+    result
 
 let part2 (input: string list) = 
-    42
+    let cave = parseCave input
+    let result = getAllFlash cave 0
+    result
