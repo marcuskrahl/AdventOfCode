86b78972f8340c71e8f59df5602abd79b6aefc82
Marcus Krahl <mail@marcuskrahl.de>
2021-12-12 16:20:25 +0100

    solve day12

--- 2021/AdventOfCode2021.Tests/Day12_Tests.fs
+++ 2021/AdventOfCode2021.Tests/Day12_Tests.fs
@@ -5,16 +5,22 @@ open Xunit
 open Day12
 
 let input = [
-
+        "start-A";
+        "start-b";
+        "A-c";
+        "A-b";
+        "b-d";
+        "A-end";
+        "b-end"
     ]
 
 
 [<Fact>]
 let ``Part 1 returns correct result`` () =
     let result = part1 input
-    Assert.Equal(42, result)
+    Assert.Equal(10, result)
 
 [<Fact>]
 let ``Part 2 returns correct result`` () =
     let result = part2 input
-    Assert.Equal(42, result)
+    Assert.Equal(36, result)
--- 2021/AdventOfCode2021/Day12.fs
+++ 2021/AdventOfCode2021/Day12.fs
@@ -2,8 +2,51 @@ module Day12
 
 open System
 
+let parseMaze (input: string list): (string * string) list = 
+    input |> List.map (fun i -> 
+            let [source; target] = List.ofSeq (i.Split("-"))
+            [(source, target); (target, source)]
+        ) |> List.concat
+
+let enumeratePaths (paths: (string * string) list) =
+    let rec enumeratePathsRec currentPath visitedNodes currentNode =
+        seq {
+            if currentNode = "end" then
+                yield (currentPath |> List.rev)
+            else
+                if visitedNodes |> List.contains currentNode then ()
+                else
+                let newVisitedNodes = if Char.IsLower(currentNode.[0]) then currentNode :: visitedNodes else visitedNodes
+                let possiblePaths = paths |> List.filter (fun (source, target) -> source = currentNode)
+                for (_, target) in possiblePaths do
+                    yield! enumeratePathsRec (target :: currentPath) newVisitedNodes target
+        }
+    enumeratePathsRec ["start"] [] "start"
+
+let enumeratePaths2 (paths: (string * string) list) =
+    let rec enumeratePathsRec currentPath visitedNodes currentNode visitedTwice =
+        seq {
+            if currentNode = "end" then
+                yield (currentPath |> List.rev)
+            else
+                if (visitedNodes |> List.contains currentNode) && ( visitedTwice = true || currentNode = "start") then ()
+                else
+                let newVisitedNodes = if Char.IsLower(currentNode.[0]) then currentNode :: visitedNodes else visitedNodes
+                let possiblePaths = paths |> List.filter (fun (source, target) -> source = currentNode)
+                let newVisitedTwice = visitedTwice || (visitedNodes |> List.contains currentNode)
+                for (_, target) in possiblePaths do
+                    yield! enumeratePathsRec (target :: currentPath) newVisitedNodes target newVisitedTwice
+        }
+    enumeratePathsRec ["start"] [] "start" false
+
 let part1 (input: string list) = 
-    42
+    let ways = input |> parseMaze
+    let paths = enumeratePaths ways |> List.ofSeq
+    printfn "%A" paths
+    paths |> List.length
 
 let part2 (input: string list) = 
-    42
+    let ways = input |> parseMaze
+    let paths = enumeratePaths2 ways |> List.ofSeq |> List.distinct |> List.sort
+    printfn "%A" paths
+    paths |> List.length 
