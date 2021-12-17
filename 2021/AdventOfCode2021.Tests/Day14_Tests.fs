566d65fa633534e9e9a1c59927a66440abf5af2a
Marcus Krahl <mail@marcuskrahl.de>
2021-12-17 13:35:23 +0100

    solve day 14

--- 2021/AdventOfCode2021.Tests/Day14_Tests.fs
+++ 2021/AdventOfCode2021.Tests/Day14_Tests.fs
@@ -5,16 +5,45 @@ open Xunit
 open Day14
 
 let input = [
-
+        "NNCB";
+        "";
+        "CH -> B";
+        "HH -> N";
+        "CB -> H";
+        "NH -> C";
+        "HB -> C";
+        "HC -> B";
+        "HN -> C";
+        "NN -> C";
+        "BH -> H";
+        "NC -> B";
+        "NB -> B";
+        "BN -> B";
+        "BB -> N";
+        "BC -> B";
+        "CC -> N";
+        "CN -> C"
     ]
+
+[<Fact>]
+let ``parses rule`` () =
+    let result = parseRule "AB -> C"
+    Assert.Equal((('A', 'B'), 'C'), result)
+
+[<Fact>]
+let ``applies rules`` () =
+    let rules = Map [ (('N', 'N'),  'C');  (('N', 'C'), 'B'); (('C', 'B'), 'H') ]
+    let result = applyRules rules (Map [ (('N', 'N'), 1L); (('N', 'C') ,1L); (('C','B'), 1L)])
+    let expected = Map [ (('N', 'C'),1L); (('C', 'N'), 1L); (('N','B'),1L); (('B','C'), 1L); (('C', 'H'), 1L); (('H', 'B'), 1L) ]
+    Assert.Equal<Map<(char * char), int64>>(expected, result)
 
 
 [<Fact>]
 let ``Part 1 returns correct result`` () =
     let result = part1 input
-    Assert.Equal(42, result)
+    Assert.Equal(1588L, result)
 
 [<Fact>]
 let ``Part 2 returns correct result`` () =
     let result = part2 input
-    Assert.Equal(42, result)
+    Assert.Equal(2188189693529L, result)
--- 2021/AdventOfCode2021/Day14.fs
+++ 2021/AdventOfCode2021/Day14.fs
@@ -1,9 +1,71 @@
 module Day14
 
 open System
+open FSharp.Collections
+
+let parseRule (ruleString: string) = 
+    let [ tupleString; resultString] = ruleString.Split(" -> ") |> List.ofSeq
+    (( tupleString.[0], tupleString.[1]),(resultString.[0]))
+
+let applyRulesForTuple rules tuple =
+    let rule = rules |> Map.tryFind tuple
+    let (c1,c2) = tuple
+    match rule with
+        | Some output -> [output; c2] 
+        | None -> [ c2; ]
+
+let incMap map key value = 
+    let existingValue = if Map.containsKey key map then Map.find key map else 0L
+    Map.add key (value + existingValue) map
+
+let applyRules rules input = 
+    let keys = input |> Map.toSeq |> Seq.map fst
+    let mutable result = Map []
+    for key in keys do 
+        let value = input |> Map.find key
+        let rule = rules |> Map.tryFind key 
+        match rule with 
+            | Some output -> 
+                let (r1, r2) = key
+                result <- incMap result (r1, output) value 
+                result <- incMap result (output, r2) value 
+            | None ->
+                result <- incMap result key value
+    result
+
+let countValues result = 
+    // |> List.concat |> List.groupBy (fun (c, sum) -> c) |> List.map (fun (c, sums) -> (c,sums |> List.map (fun (_,s) -> s) |> List.sum))
+    result 
+        |> Map.toList 
+        |> List.map (fun ((c1,c2), sum) -> [ (c1, sum); (c2, sum)]) 
+        |> List.transpose 
+        |> List.map (fun posList -> posList |> List.groupBy (fun (c, _) -> c) |> List.map (fun (c, rest) -> (c, rest |> List.map (fun (_,s) -> s) |> List.sum ))) 
+        |> List.concat
+        |> List.groupBy (fun (c,_) -> c) 
+        |> List.map (fun (c, sums) -> (c, sums |> List.map (fun (_, s) -> s) |> List.max))
 
 let part1 (input: string list) = 
-    42
+    let first = (input |> List.head).ToCharArray() |> List.ofSeq |> List.pairwise |> List.map (fun t -> (t, 1L)) |> Map.ofList
+    let rules = input |> List.skip 2 |> List.map parseRule |> Map.ofList
+    let mutable result = first
+    printfn "%A" (countValues result)
+    for i in 1..10 do
+        result <- applyRules rules result
+        printfn "%A" (countValues result)
+    let groups = countValues result
+    let (_,max) = groups |> List.maxBy (fun (_,v) -> v)
+    let (_,min) = groups |> List.minBy (fun (_,v) -> v)
+    max - min
 
 let part2 (input: string list) = 
-    42
+    let first = (input |> List.head).ToCharArray() |> List.ofSeq |> List.pairwise |> List.map (fun t -> (t, 1L)) |> Map.ofList
+    let rules = input |> List.skip 2 |> List.map parseRule |> Map.ofList
+    let mutable result = first
+    printfn "%A" (countValues result)
+    for i in 1..40 do
+        result <- applyRules rules result
+        printfn "%A" (countValues result)
+    let groups = countValues result
+    let (_,max) = groups |> List.maxBy (fun (_,v) -> v)
+    let (_,min) = groups |> List.minBy (fun (_,v) -> v)
+    max - min
