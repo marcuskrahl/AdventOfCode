db2fcd3682ef8ee0bc5a1bcbe71526bc43ca3214
Marcus Krahl <mail@marcuskrahl.de>
2021-12-18 11:27:36 +0100

    solve day 17

--- 2021/AdventOfCode2021.Tests/Day17_Tests.fs
+++ 2021/AdventOfCode2021.Tests/Day17_Tests.fs
@@ -5,16 +5,33 @@ open Xunit
 open Day17
 
 let input = [
-
+        "target area: x=20..30, y=-10..-5"
     ]
 
+[<Fact>]
+let ``parse area`` () =
+    let result = input |> List.head |> parseInput
+    Assert.Equal(((20,30), (-10,-5)), result)
+
+[<Fact>]
+let ``reaches area`` () =
+    let area = (20,30),(-10,-5)
+    Assert.Equal(true, reachesArea (0,0) (7,2) area)
+    Assert.Equal(true, reachesArea (0,0) (6,3) area)
+    Assert.Equal(true, reachesArea (0,0) (9,0) area)
+    Assert.Equal(false, reachesArea (0,0) (17,-4) area)
+
+[<Fact>]
+let ``reaches area and velocity`` () =
+    let area = (20,30),(-10,-5)
+    Assert.Equal((true,45), reachesAreaAndVelocity (0,0) (6,9) area 0)
 
 [<Fact>]
 let ``Part 1 returns correct result`` () =
     let result = part1 input
-    Assert.Equal(42, result)
+    Assert.Equal(45, result)
 
 [<Fact>]
 let ``Part 2 returns correct result`` () =
     let result = part2 input
-    Assert.Equal(42, result)
+    Assert.Equal(112, result)
--- 2021/AdventOfCode2021/Day17.fs
+++ 2021/AdventOfCode2021/Day17.fs
@@ -2,8 +2,51 @@ module Day17
 
 open System
 
+let parseInput (input: string) = 
+    let [|xRange; yRange|] = input.Replace("target area: x=", "").Replace(" y=","").Split(",")
+    let [xMin; xMax] = xRange.Split("..") |> List.ofSeq |> List.map (fun n -> Convert.ToInt32(n))
+    let [yMin; yMax] = yRange.Split("..") |> List.ofSeq |> List.map (fun n -> Convert.ToInt32(n))
+    (xMin,xMax),(yMin,yMax) 
+
+let rec reachesAreaAndVelocity (x,y) (vx,vy) area reachedY = 
+    let (xMin, xMax), (yMin, yMax) = area
+    if x > xMax then (false,reachedY)
+    else if y < yMin && vx = 0 then (false,reachedY)
+    else if x >= xMin && x <= xMax && y >= yMin && y <= yMax then (true, reachedY)
+    else
+        let newPos = (x + vx), (y+vy)
+        let newVx = if vx > 0 then vx - 1 else if vx < 0 then vx + 1 else 0
+        let newVy = vy - 1
+        let newReachedY = if (y+vy) > reachedY then y+vy else reachedY
+        reachesAreaAndVelocity newPos (newVx, newVy) area newReachedY
+
+let reachesArea (x,y) (vx,vy) area = 
+    let (result, _) = reachesAreaAndVelocity (x,y) (vx,vy) area y
+    result
+
+
+
+let highestTrajectory area =
+    let mutable highest = 0
+    for vx in 0..100 do
+        for vy in 0..100 do 
+            let (result, yMax) = reachesAreaAndVelocity (0,0) (vx,vy) area 0 
+            if result = true && yMax > highest then highest <- yMax else () 
+    highest
+
+let allTrajectory area =
+    let mutable count = 0
+    for vx in 0..300 do
+        for vy in -100..100 do 
+            let (result, yMax) = reachesAreaAndVelocity (0,0) (vx,vy) area 0 
+            if result = true then count <- count + 1 else () 
+    count
+
+
 let part1 (input: string list) = 
-    42
+    let area = input |> List.head |> parseInput
+    highestTrajectory area
 
 let part2 (input: string list) = 
-    42
+    let area = input |> List.head |> parseInput
+    allTrajectory area
