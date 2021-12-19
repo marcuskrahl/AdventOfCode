b13622bad8f2e661858b7b592cc33d87f1390e64
Marcus Krahl <mail@marcuskrahl.de>
2021-12-19 10:51:01 +0100

    solve day18

--- 2021/AdventOfCode2021.Tests/Day18_Tests.fs
+++ 2021/AdventOfCode2021.Tests/Day18_Tests.fs
@@ -5,16 +5,59 @@ open Xunit
 open Day18
 
 let input = [
-
+        "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]";
+        "[[[5,[2,8]],4],[5,[[9,9],0]]]";
+        "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]";
+        "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]";
+        "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]";
+        "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]";
+        "[[[[5,4],[7,7]],8],[[8,3],8]]";
+        "[[9,3],[[9,9],[6,[4,9]]]]";
+        "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]";
+        "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
     ]
+
+let testExplode input result = 
+    let pair = input |> parsePair
+    let (r,_) = explode pair
+    let resultStr = r |> pairToString
+    Assert.Equal(result, resultStr)
+
+let testSplit input result = 
+    let pair = input |> parsePair
+    let (r,_) = split pair
+    let resultStr = r |> pairToString
+    Assert.Equal(result, resultStr)
+
+let testReduce input result = 
+    let pair = input |> parsePair
+    let r = reducePair pair
+    let resultStr = r |> pairToString
+    Assert.Equal(result, resultStr)
+
+[<Fact>]
+let ``explodes correctly`` () =
+    testExplode "[[[[[9,8],1],2],3],4]" "[[[[0,9],2],3],4]"
+    testExplode "[7,[6,[5,[4,[3,2]]]]]" "[7,[6,[5,[7,0]]]]"
+    testExplode "[[6,[5,[4,[3,2]]]],1]" "[[6,[5,[7,0]]],3]"
+    testExplode "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]" "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+    testExplode "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]" "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+
+[<Fact>]
+let ``splits correctly`` () =
+    testSplit "[11,1]" "[[5,6],1]"
+
+[<Fact>]
+let ``reduces correctly`` () =
+    testReduce "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]" "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
 
 
 [<Fact>]
 let ``Part 1 returns correct result`` () =
     let result = part1 input
-    Assert.Equal(42, result)
+    Assert.Equal(4140L, result)
 
 [<Fact>]
 let ``Part 2 returns correct result`` () =
     let result = part2 input
-    Assert.Equal(42, result)
+    Assert.Equal(3993L, result)
--- 2021/AdventOfCode2021/Day18.fs
+++ 2021/AdventOfCode2021/Day18.fs
@@ -2,8 +2,260 @@ module Day18
 
 open System
 
+type Pair = PairValue * PairValue
+    and PairValue =
+        | Value of int
+        | NestedPair of Pair
+
+let rec parsePair (input: string) = 
+    let rec parsePart (part:string) = 
+        if part.StartsWith("[") 
+        then NestedPair (parsePair part)
+        else 
+            let v = Convert.ToInt32(part)
+            Value v
+
+    let mutable commaPosition = 0
+    let mutable layer = 0;
+    for i in 0..(input.Length - 1) do
+        let c = input.[i]
+        match c with 
+            | ',' when layer = 1 -> commaPosition <- i
+            | '[' -> layer <- layer + 1
+            | ']' -> layer <- layer - 1
+            | _ -> ()
+    let firstPart = input.Substring(1, commaPosition - 1)
+    let secondPart = input.Substring(commaPosition + 1, input.Length - commaPosition - 2)
+    ( parsePart firstPart), (parsePart secondPart)
+
+let add one two = 
+    NestedPair (one, two)
+
+let rec pairToString pair = 
+    let (left, right) = pair
+    let leftStr = match left with
+        | Value v -> $"{v}"
+        | NestedPair p -> $"{pairToString p}"
+    let rightStr = match right with
+        | Value v -> $"{v}"
+        | NestedPair p -> $"{pairToString p}"
+    $"[{leftStr},{rightStr}]"
+
+
+let getValue v =
+    match v with
+        | Value a -> a
+        | _ -> raise (System.ArgumentException("expected value"))
+
+let getPair p =
+    match p with
+        | NestedPair a -> a
+        | _ -> raise (System.ArgumentException("expected pair"))
+
+let explode completePair = 
+    let rec getExplodePosition pair position = 
+        if position |> List.length >= 4 
+        then Some ((position |> List.rev), pair)
+        else
+            let (left, right) = pair
+            let leftResult = match left with 
+                | Value _ -> None
+                | NestedPair p -> getExplodePosition p (0::position)
+            if leftResult |> Option.isSome then leftResult
+            else 
+            match right with
+                | Value _ -> None
+                | NestedPair p -> getExplodePosition p (1::position)
+
+
+    let rec incPosition position value pair = 
+        //printfn "inc position %A %d" position value
+        match position with 
+            | [0] -> 
+                let (leftValue, rightValue) = pair
+                let newLeftValue = (leftValue |> getValue) + value
+                (Value newLeftValue, rightValue)
+            | [1] ->    
+                let (leftValue, rightValue) = pair
+                let newRightValue = (rightValue |> getValue) + value
+                //printfn "new right value %d" newRightValue
+                (leftValue, Value newRightValue)
+            | 0::rest -> 
+                let (leftPair, rightPair) = pair 
+                let newLeftPair = incPosition rest value (leftPair |> getPair)
+                (NestedPair newLeftPair, rightPair)
+            | 1::rest -> 
+                let (leftPair, rightPair) = pair 
+                let newRightPair = incPosition rest value (rightPair |> getPair)
+                (leftPair, NestedPair newRightPair)
+
+    let getAllPositions pair = 
+        let rec getAllPositionsRec pair position = 
+            seq { 
+                let (left, right) = pair
+                match left with 
+                    | Value v -> 
+                        yield ((0::position) |> List.rev)
+                    | NestedPair p -> 
+                        yield! getAllPositionsRec p (0::position)
+                match right with 
+                    | Value v -> 
+                        yield ((1::position) |> List.rev)
+                    | NestedPair p -> 
+                        yield! getAllPositionsRec p (1::position)
+            }
+        (getAllPositionsRec pair []) |> List.ofSeq
+
+    let tryExplodeLeft explodePosition leftValue pair =
+        let isSmallerPosition position = 
+            let rec isSmallerPositionRec l1 l2 = 
+                if List.isEmpty l1 && List.isEmpty l2 then false
+                else if List.isEmpty l1 then false
+                else if List.isEmpty l2 then false
+                else
+                let (e1 :: r1) = l1;
+                let (e2 :: r2) = l2;
+                if e1 < e2 then true
+                else if e1 > e2 then false
+                else isSmallerPositionRec r1 r2
+               
+            isSmallerPositionRec position explodePosition
+
+        let getLeftPosition position pair = 
+            let allPositions = getAllPositions pair
+            let smallerPositions = allPositions |> List.filter isSmallerPosition
+            //printfn "%A" smallerPositions
+            if smallerPositions |> List.isEmpty 
+            then None
+            else Some (smallerPositions |> List.last)
+            
+        let leftPosition = getLeftPosition explodePosition pair
+        match leftPosition with 
+            | Some p -> incPosition p leftValue pair
+            | None -> pair
+
+    let tryExplodeRight explodePosition rightValue pair =
+        let isLargerPosition position = 
+            let rec isLargerPositionRec l1 l2 = 
+                if List.isEmpty l1 && List.isEmpty l2 then false
+                else if List.isEmpty l1 then false
+                else if List.isEmpty l2 then false
+                else
+                let (e1 :: r1) = l1;
+                let (e2 :: r2) = l2;
+                if e1 > e2 then true
+                else if e1 < e2 then false
+                else isLargerPositionRec r1 r2
+               
+            isLargerPositionRec position explodePosition
+
+        let getRightPosition position pair = 
+            let allPositions = getAllPositions pair
+            let largerPositions = allPositions |> List.filter isLargerPosition
+            //printfn "right: %A" largerPositions
+            if largerPositions |> List.isEmpty 
+            then None
+            else Some (largerPositions |> List.head)
+            
+        let rightPosition = getRightPosition explodePosition pair
+        match rightPosition with 
+            | Some p -> incPosition p rightValue pair
+            | None -> pair
+
+    let tryExplodeRemaining explodePosition pair = 
+        let rec tryExplodeRec position pair = 
+            //printfn "explode %A %A" position pair
+            let (left, right) = pair
+            match position with
+                | [0] -> (Value 0, right)
+                | [1] -> (left, Value 0)
+                | 0 :: rest -> 
+                    let newLeft = tryExplodeRec rest (left |> getPair)
+                    (NestedPair newLeft, right)
+                | 1 :: rest -> 
+                    let newRight = tryExplodeRec rest (right |> getPair)
+                    (left, NestedPair newRight)
+
+        tryExplodeRec explodePosition pair
+
+    let explodeResult = getExplodePosition completePair []
+    let result = match explodeResult with
+        | Some (position, (leftValue, rightValue)) ->
+            //printfn "%A %A %A" position leftValue rightValue
+            let result = 
+                completePair 
+                    |> tryExplodeLeft position (leftValue |> getValue)
+                    |> tryExplodeRight position (rightValue |> getValue)
+                    |> tryExplodeRemaining position
+            //printfn "explode result: %A" result
+            (result, true)
+        | None -> 
+            //printfn "nothing to explode"
+            (completePair, false)
+    result
+
+let splitValue v = 
+    let v1 = v / 2;
+    let v2 = v1 + if v % 2 = 1 then 1 else 0;
+    NestedPair (Value v1, Value v2)
+
+let rec split pair =
+    let (left, right) = pair
+    let (newLeft, leftResult) = match left with
+        | Value v when v > 9 -> (splitValue v, true)
+        | Value v when v <= 9 -> (Value v, false)
+        | NestedPair p -> 
+            let (newPair, res) = split p
+            (NestedPair newPair, res)
+    if leftResult = true then
+        ((newLeft, right), true)
+    else 
+    let (newRight, rightResult) = match right with
+        | Value v when v > 9 -> (splitValue v, true)
+        | Value v when v <= 9 -> (Value v, false)
+        | NestedPair p ->
+            let (newPair, res) = split p
+            (NestedPair newPair, res)
+    ((left, newRight), rightResult)
+
+let rec reducePair pair = 
+    let (explodeResult, didExplode) = explode pair
+    if didExplode then 
+        printfn "did explode %s" (explodeResult |> pairToString)
+        reducePair explodeResult
+    else 
+    let (splitResult, didSplit) = split pair
+    if didSplit then
+        printfn "did split %s" (splitResult |> pairToString)
+        reducePair splitResult
+    else 
+    pair
+
+let addPair p1 p2 = 
+    (NestedPair p1, NestedPair p2)
+
+let rec magnitude (left,right) = 
+    let leftValue = match left with
+     | Value v -> int64 v
+     | NestedPair p -> magnitude p
+    let rightValue = match right with
+     | Value v -> int64 v
+     | NestedPair p -> magnitude p
+    leftValue * 3L + rightValue * 2L
+
+let highestMagnitude numbers = 
+    List.allPairs numbers numbers |> List.map (fun (n1, n2) -> 
+        if n1 = n2 then 0L
+        else addPair n1 n2 |> reducePair |> magnitude
+    ) |> List.max
+
 let part1 (input: string list) = 
-    42
+    let pairs = input |> List.map parsePair 
+    let firstPair::restPairs = pairs 
+    let result = restPairs |> List.fold (fun acc curr -> curr |> addPair acc |> reducePair) firstPair
+    printfn "%s" (result |> pairToString)
+    magnitude result
 
 let part2 (input: string list) = 
-    42
+    let pairs = input |> List.map parsePair 
+    highestMagnitude pairs
