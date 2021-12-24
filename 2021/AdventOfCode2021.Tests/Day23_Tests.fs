09d82d4fc04a3769c5f6c99cccaa03cfa7c3d5b0
Marcus Krahl <mail@marcuskrahl.de>
2021-12-24 08:17:23 +0100

    fail at day 23

--- 2021/AdventOfCode2021.Tests/Day23_Tests.fs
+++ 2021/AdventOfCode2021.Tests/Day23_Tests.fs
@@ -5,14 +5,14 @@ open Xunit
 open Day23
 
 let input = [
-
+    
     ]
 
 
 [<Fact>]
 let ``Part 1 returns correct result`` () =
     let result = part1 input
-    Assert.Equal(42, result)
+    Assert.Equal(12521, result)
 
 [<Fact>]
 let ``Part 2 returns correct result`` () =
--- 2021/AdventOfCode2021/Day23.fs
+++ 2021/AdventOfCode2021/Day23.fs
@@ -2,8 +2,68 @@ module Day23
 
 open System
 
+type Position = 
+    | Hallway of int32
+    | Room of (char * int32)
+    
+
+type Amphipod = {
+        Name : char
+        Position : Position ;
+        Final : bool;
+    }
+
+let amphipods = [
+        { Name= 'A'; Position= Room('A', 2); Final= false };
+        { Name= 'A'; Position= Room('D', 2); Final= false };
+        { Name= 'B'; Position= Room('A', 1); Final= false };
+        { Name= 'B'; Position= Room('C', 1); Final= false };
+        { Name= 'C'; Position= Room('B', 1); Final= false };
+        { Name= 'C'; Position= Room('C', 2); Final= true };
+        { Name= 'D'; Position= Room('D', 1); Final= false };
+        { Name= 'D'; Position= Room('B', 2); Final= false };
+    ]
+
+let IMPOSSIBLE = 999999999;
+
+let rec move pods cost = 
+    let isOccupied c r = 
+        pods |> List.exists (fun p -> p.Position = Room(c,r))
+
+    let isOccupiedHallway hallway = 
+        pods |> List.exists (fun p -> p.Position = Hallway(hallway))
+
+    let canMove hallway room = 
+        let occupiedHallways = pods |> List.map 
+        (fun p -> 
+                match p.Position with
+                    | Hallway(h) -> h
+                    | _ -> -42
+            ) |> List.where (fun h -> h > -42)
+
+
+    let moveHallway pod = 
+        //get available spots
+        42
+
+    let moveRoom pod room =
+        //check if empty
+        42
+
+    let tryMove pod = 
+        match pod.Position with 
+            | Room(c,2) when (isOccupied c 1) -> IMPOSSIBLE 
+            | Room(c,2) when c <> pod.Name -> moveHallway pod  
+            | Room(c,1) when c <> pod.Name -> moveHallway pod  
+            | Hallway(p) when canMove p pod.Name -> moveRoom pod pod.Name
+            | _ -> IMPOSSIBLE
+        
+    let movablePods = pods |> List.where (fun p -> p.Final <> true)
+    let childResult = movablePods |> List.map (tryMove) |> List.min 
+    cost + childResult
+
 let part1 (input: string list) = 
-    42
+    move amphipods 0
 
 let part2 (input: string list) = 
     42
