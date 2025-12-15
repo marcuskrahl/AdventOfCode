9e906fb00a92bc6229ff48ae76b33a236bfb6aca
Marcus Krahl <mail@marcuskrahl.de>
2025-12-15 18:03:15 +0100

    solve day 11 2025 part 2

--- 2025/day11.ts
+++ 2025/day11.ts
@@ -12,21 +12,34 @@ function parseConnnections(input: string): Connections {
   return connections;
 }
 
-function getConnections(connections: Connections, input: string): number {
+let cache: {[key: string]: number} = {};
+
+function getConnections(connections: Connections, input: string, dac: boolean, fft: boolean): number {
+  const cacheKey = `${input}${dac ? '_T': '_F'}${fft ? '_T' : '_F'}`;
+  if (cache[cacheKey]!=undefined) {
+    return cache[cacheKey];
+  }
   if (input === 'out') {
-    return 1;
+    return dac && fft ? 1: 0;
   }
   const outputs = connections[input];
-  return sum(outputs.map(o => getConnections(connections, o)))
+  const result = sum(outputs.map(o => getConnections(connections, o, dac || input === "dac",fft || input === "fft")))
+  cache[cacheKey] = result;
+  return result;
 }
 
 export function part1(input: string) {
+  cache = {};
   const connections = parseConnnections(input);
-  return getConnections(connections, 'you');
+  return getConnections(connections, 'you', true, true);
 }
 
 export function part2(input: string) {
-  return 0;
+  cache = {};
+  const connections = parseConnnections(input);
+  const result =  getConnections(connections, 'svr',false, false);
+  //console.log(cache);
+  return result;
 }
 
 
@@ -47,6 +60,22 @@ Deno.test("part1", async () => {
   assertEquals(part1(sampleInput), 5);
 });
 
+const sampleInput2 = `
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+`.trim();
+
 Deno.test("part2", async () => {
-  assertEquals(part2(sampleInput), 0);
+  assertEquals(part2(sampleInput2), 2);
 });
