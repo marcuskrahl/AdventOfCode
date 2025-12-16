e5fd443a8d851075ada58480469089705a48657e
Marcus Krahl <mail@marcuskrahl.de>
2025-12-16 18:53:04 +0100

    solve day 9 2025 part 2

--- 2025/day09.ts
+++ 2025/day09.ts
@@ -114,10 +114,154 @@ function subdivideArea(coords: Coord[]): number {
   let maxArea = max(areas.filter(({from, to}) => from.x != 0 && from.y != 0 && from.x !== 100_000 && from.y !== 100_000 
   && to.x != 0 && to.y != 0 &&to.x !== 100_000 && to.y !== 100_000 ), 
   ({from ,to}) => (Math.abs(from.x - to.x) + 1) * (Math.abs(from.y - to.y) + 1));
-  console.log
-  console.log(areas, maxArea);
+  //console.log
+  //console.log(areas, maxArea);
   return (Math.abs(maxArea.from.x - maxArea.to.x) + 1) * (Math.abs(maxArea.from.y - maxArea.to.y) + 1);
 };
+
+// The coordinates here have to be probably also reversed because we are only expanding every edge in one dimension while we probably need both dimensions.
+// But it gets the job done for my input ;)
+function cornerExtension(coords: Coord[]): number {
+
+  let dividers = [];
+  for (let i = 1; i < coords.length; i++) {
+    dividers.push({from: coords[i-1], to: coords[i], horizontal: coords[i-1].y === coords[i].y});
+  }
+  dividers.push({from: coords.at(-1)!, to: coords[0], horizontal: coords.at(-1)!.y === coords[0].y});
+  const horizontalDividers = dividers.filter(d => d.horizontal);
+  const verticalDividers = dividers.filter(d => !d.horizontal);
+
+  let maxArea = 0;
+    for (let i=1; i < dividers.length; i++) {
+      const d1 = dividers[i-1];
+      const d2 = dividers[i];
+
+
+
+      const from = { x: d1.to.x, y: d1.to.y };
+      let x = from.x - Math.sign(d1.to.x - d1.from.x) / 2;
+      let y = from.y - Math.sign(d1.to.y - d1.from.y) / 2;
+
+      //find next corner; 
+      let dx = Math.sign(d2.to.x - d2.from.x);
+      let dy = Math.sign(d2.to.y - d2.from.y);
+      let wrongCorner = false;
+      while(true) {
+        x += dx;
+        y += dy;
+        if (x <=0) {
+          wrongCorner = true;
+          break;
+        }
+        if (x >= 100_000) {
+          wrongCorner = true;
+          break;
+        }
+        if (y <= 0) {
+          wrongCorner = true;
+          break;
+        }
+        if (y >= 100_000) {
+          wrongCorner = true;
+          break;
+        }
+        if (!d1.horizontal && verticalDividers.some(d => d.from.x === x && d.from.y <= y && d.to.y >= y)) {
+          //reached edge
+          break;
+        }
+        if (d1.horizontal && horizontalDividers.some(d => d.from.y === y && d.from.x <= x && d.to.x >= x)) {
+          //reached edge
+          break;
+        }
+      }
+      if (wrongCorner) {
+        continue;
+      }
+      
+
+
+      x += Math.sign(d1.to.x - d1.from.x) / 2;
+      y += Math.sign(d1.to.y - d1.from.y) / 2;
+
+      const ox = x;
+      const oy = y;
+      //console.log(d1,d2);
+      //console.log('opposite corner ', x,y);
+
+      dx = -1 * Math.sign(d1.to.x - d1.from.x);
+      dy = -1 * Math.sign(d1.to.y - d1.from.y);
+
+      let rx = 0;
+      let ry = 0;
+
+      while(true) {
+        x += dx;
+        y += dy;
+        if (x <=0) {
+          wrongCorner = true;
+          break;
+        }
+        if (x >= 100_000) {
+          wrongCorner = true;
+          break;
+        }
+        if (y <= 0) {
+          wrongCorner = true;
+          break;
+        }
+        if (y >= 100_000) {
+          wrongCorner = true;
+          break;
+        }
+        const matchingFrom = coords.find(d => d.x === x && d.y === y || d1.horizontal && d.x === x && d.y === from.y || !d1.horizontal && d.x === from.x && d.y === y);
+        if (matchingFrom != undefined) {
+          rx = matchingFrom.x;
+          ry = matchingFrom.y;
+          //console.log('result corner', rx,ry);
+        }
+        if (!d1.horizontal && horizontalDividers.some(d => d.from.y === y && !(Math.max(d.from.x, d.to.x) <= Math.min(from.x, ox) || Math.min(d.from.x,d.to.x) >= Math.max(from.x, ox)))) {
+          break;
+        }
+         if (d1.horizontal && verticalDividers.some(d => d.from.x === x && !(Math.max(d.from.y, d.to.y) <= Math.min(from.y, oy) ||  Math.min(d.from.y,d.to.y) >= Math.max(from.y, oy)))) {
+          break;
+        }
+        /*if (dividers.some(d => d.from.x === x && d.from.y === y) )
+        if (!d1.horizontal && verticalDividers.some(d => d.from.x === x && d.from.y <= y && d.to.y >= y)) {
+          //reached edge
+          break;
+        }
+        if (d1.horizontal && horizontalDividers.some(d => d.from.y === y && d.from.x <= x && d.to.x >= x)) {
+          //reached edge
+          break;
+        }*/
+      }
+      if (wrongCorner) {
+        continue;
+      }
+      if (rx === 0 || ry === 0) {
+        continue;
+      }
+      if (d1.horizontal && ry === from.y) {
+        from.y = oy;
+      }
+      if (!d1.horizontal && rx === from.x) {
+        from.x = ox;
+      }
+      if (!coords.some(c => c.x === from.x && c.y === from.y)) {
+        continue;
+      }
+      if (!coords.some(c => c.x === rx && c.y === ry)) {
+        continue;
+      }
+      const area = (Math.abs (from.x - rx) + 1) * (Math.abs(from.y - ry) + 1);
+      if (area > maxArea) {
+      console.log(area);
+        maxArea = area;
+      }
+    }
+  
+  return maxArea;
+}
 
 export function part1(input: string) {
   const coords = parseCoords(input);
@@ -126,7 +270,7 @@ export function part1(input: string) {
 
 export function part2(input: string) {
   const coords = parseCoords(input);
-  return subdivideArea(coords);
+  return cornerExtension(coords);
 }
 
 
