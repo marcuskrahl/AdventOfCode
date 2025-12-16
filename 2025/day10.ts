958e4ff664583664751d8b96f3169d88b3c70e58
Marcus Krahl <mail@marcuskrahl.de>
2025-12-16 17:48:31 +0100

    visualize day 9 2025

--- 2025/day10.ts
+++ 2025/day10.ts
@@ -102,7 +102,7 @@ function minJoltage2(machine: Machine): number {
 
     let targetButtons = buttons.filter(b => b.includes(targetIndex));
 
-    console.log(targetButtons);
+    //console.log(targetButtons);
     const rec = () => {
 
     }
@@ -116,6 +116,126 @@ function minJoltage2(machine: Machine): number {
     return 0;
 }
 
+function press(button: number[], joltages: number[], presses: number = 1) :void{
+    for (let i  = 0; i < button.length; i++) {
+        joltages[button[i]] -= presses;
+    }
+}
+
+
+function minJoltage3(buttons: number[][], joltages: number[], buttonSlice: number = 0): number {
+    //let [button, ...rest]= buttons;
+    //console.log(button, rest);
+    let maxPresses = Math.min(...joltages.filter((_,i) => buttons[buttonSlice].includes(i)));
+    //console.log(button, maxPresses, joltages);
+    if (buttonSlice >= buttons.length - 1) {
+        press(buttons[buttonSlice], joltages, maxPresses);
+        const result = joltages.some(j => j > 0) ? Number.MAX_SAFE_INTEGER : maxPresses;
+        press(buttons[buttonSlice], joltages, -maxPresses);
+        return result;
+    }
+    for (let j = 0 ; j < joltages.length; j++) {
+        if (joltages[j] === 0) {
+            continue;
+        }
+        let found = false
+        for (let i =buttonSlice; i<buttons.length; i++) {
+            for (let k = 0; k < buttons[buttonSlice].length; k++) {
+                if (buttons[i][k] === j) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found) {
+            return Number.MAX_SAFE_INTEGER;
+        }
+    }
+    
+
+    let minPresses = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i <= maxPresses; i++) {
+        if (buttonSlice === 0) {
+            console.log('b', i / maxPresses * 100 );
+
+        }
+        press(buttons[buttonSlice], joltages, i);
+        const subPresses = i + minJoltage3(buttons, joltages, buttonSlice + 1);
+        press(buttons[buttonSlice], joltages, -i);
+        if (subPresses < minPresses) {
+            //console.log(button, rest, newJoltage, subPresses);
+            minPresses = subPresses;
+        }
+    }
+    return minPresses;
+
+}
+
+function optimizeMachine(buttons: number[][], joltages: number[]) : [number[][], number[]] {
+    /*let newButtons = [...buttons];
+    let newJoltage = [...joltages];
+    let joltageDistribution = joltages.map((v, i) => buttons.filter(b => b.includes(i))).filter(d => d.length === 1);
+    for (let b of joltageDistribution) {
+        console.log(b);
+    }*/
+   /*let twoButtonCombination: undefined | number[][] = undefined;
+   let threeButtonCombination: undefined | number[][] = undefined
+   for (let b1 of buttons) {
+        if (twoButtonCombination != undefined) {
+            break;
+        }
+        for (let b2 of buttons) {
+            if (b1 == b2) {
+                continue;
+            }
+            if (b2.some(b => b1.includes(b))) {
+                continue;
+            }
+            if (b1.length + b2.length === joltages.length) {
+                twoButtonCombination = [b1,b2];
+                break;
+            }
+            for (let b3 of buttons) {
+                if (b3 == b2 || b3 == b1) {
+                    continue;
+                }
+                if (b3.some( b => b1.includes(b))) {
+                    continue;
+                }
+                if (b3.some( b => b2.includes(b))) {
+                    continue;
+                }
+                if (b1.length + b2.length + b3.length === joltages.length) {
+                    threeButtonCombination = [b1,b2, b3];
+                    break;
+                }
+            }
+        }
+   }
+   if (twoButtonCombination != undefined) {
+    console.log('two button', twoButtonCombination);
+    return [twoButtonCombination, joltages];
+   }
+   if (threeButtonCombination != undefined) {
+    console.log('three button');
+    return [threeButtonCombination, joltages];
+   }*/
+
+   return [buttons, joltages];
+}
+
+
+
+function sortButtons(buttons: number[][], joltages: number[]): number[][] {
+    const result = buttons.slice(0);
+    const cardinality = Object.fromEntries(joltages.map((v,i)=> [i, buttons.filter(b => b.includes(i)).length]))
+    return result.toSorted((b1,b2) => {
+        const b1Card = Math.min(...b1.map(b => cardinality[b]));
+        const b2Card =  Math.min(...b2.map(b => cardinality[b]));
+        return b1Card - b2Card;
+    });
+}
+
 export function part1(input: string) {
     const machines = getLines(input).map(i => parseMachine(i));
     return sum(machines.map(m => minPresses(m)));
@@ -123,7 +243,13 @@ export function part1(input: string) {
 
 export function part2(input: string) {
   const machines = getLines(input).map(i => parseMachine(i));
-  return sum(machines.map(m => minJoltage2(m)));
+  //console.log(machines.filter(m => m.buttonsExtended.length > m.joltages.length).length, machines.length)
+  return sum(machines.map((m,i) => {
+    let [buttons, joltages] = optimizeMachine(m.buttonsExtended, m.joltages);
+    const res = minJoltage3(sortButtons(m.buttonsExtended, m.joltages), m.joltages);
+    console.log(i + 1, res); 
+    return res; 
+    }) );
 }
 
 
